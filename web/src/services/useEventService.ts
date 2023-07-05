347bc9a20c5d4418c3cea84fb7d794472f3c5b7f
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-07-05 12:31:19 -0400

    web - copyright

--- web/src/services/useEventService.ts
+++ web/src/services/useEventService.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023 Asim Ihsan.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { useEffect } from 'react';
 import { LogEvent, HttpRequestEvent, KafkaMessageEvent } from '../models/Event';
 import { useEventContext } from '../utils/EventContext';
